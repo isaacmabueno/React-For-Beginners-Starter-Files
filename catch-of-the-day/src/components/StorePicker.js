ee85d1c092789855b7481b4b7a74c3fddce6c8cf
isaacmabueno <isaacmabueno@gmail.com>
2018-10-08 20:54:13 -0500

    writing html with jsx

--- catch-of-the-day/src/components/StorePicker.js
+++ catch-of-the-day/src/components/StorePicker.js
@@ -1,8 +1,15 @@
 import React from 'react';
+import { format } from 'url';
 
 class StorePicker extends React.Component {
     render() {
-        return <p>HEOOOOOOO</p>
+        return (
+            <React.Fragment>
+                <h2>Please Enter A Store</h2>    
+                <input type='text' required placeholder='Store Name'></input>
+                <button type='submit'>Visit Store &rarr;</button>
+            </React.Fragment>
+        )
     }
 }
 
