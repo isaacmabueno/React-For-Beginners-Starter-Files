fb6dbfee7915522e3cfc09fcfd995c2800effbad
isaacmabueno <isaacmabueno@gmail.com>
2018-10-14 23:38:23 -0500

    starting submit and click events

--- catch-of-the-day/src/components/StorePicker.js
+++ catch-of-the-day/src/components/StorePicker.js
@@ -2,12 +2,23 @@ import React from 'react';
 import { format } from 'url';
 
 class StorePicker extends React.Component {
+
+    goToStore() {
+        console.log('hey');
+    }
     render() {
         return (
             <React.Fragment>
-                <h2>Please Enter A Store</h2>    
-                <input type='text' required placeholder='Store Name'></input>
+            <form className='store-selector' onSelect={this.goToStore}>
+                <h2>Please Enter A Store</h2>   
+                <input 
+                    type='text' 
+                    required 
+                    placeholder='Store Name' 
+                    defaultValue={getFunName()}
+                />
                 <button type='submit'>Visit Store &rarr;</button>
+            </form>
             </React.Fragment>
         )
     }
