827fe0ca1f9b384d2dff335e41fb63a4ea47f888
isaacmabueno <isaacmabueno@gmail.com>
2018-10-14 23:16:23 -0500

    routing with react router

---
+++ catch-of-the-day/src/components/NotFound.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const NotFound = () => (
+    <React.Fragment>
+        <div>
+            <h1>Sorry, but this page doesn't exist. </h1>
+            <p>Double check your URL or check back later!</p>
+        </div>
+    </React.Fragment>
+)
+
+export default NotFound
