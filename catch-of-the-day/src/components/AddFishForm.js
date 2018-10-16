cb98d2a29bf1b8a9557c860298e5680173459b26
isaacmabueno <isaacmabueno@gmail.com>
2018-10-15 23:36:49 -0500

    creating fish object

--- catch-of-the-day/src/components/AddFishForm.js
+++ catch-of-the-day/src/components/AddFishForm.js
@@ -1,20 +1,38 @@
 import React from 'react'
 
 class AddFishForm extends React.Component {
-    createFish = () => {
-        alert('hello')
-    }
+
+    nameRef =  React.createRef();
+    priceRef =  React.createRef();
+    statusRef =  React.createRef();
+    descRef =  React.createRef();
+    imageRef =  React.createRef();
+    createFish = event => {
+        // 1. stop the form from submitting
+        event.preventDefault();
+
+        const fish = {
+            name: this.nameRef.value.value,
+            price: parseFloat(this.priceRef.value.value),
+            status: this.statusRef.value.value,
+            desc: this.descRef.value.value,
+            image: this.imageRef.value.value
+        }
+        console.log('MAKING A 🐟');
+
+        console.log(fish);
+    };
     render() {
         return (
             <form onSubmit={this.createFish}>
-                <input name="name" type="text" placeholder="Name"/>
-                <input name="price" type="text" placeholder="Price"/>
-                <select name="status">
+                <input name="name"ref={this.nameRef} type="text" placeholder="Name"/>
+                <input name="price"ref={this.priceRef} type="text" placeholder="Price"/>
+                <select name="status"ref={this.statusRef}>
                     <option value="available">Fresh!</option>
                     <option value="unavailable">Sold Out!</option>
                 </select>
-                <textarea name="desc" placeholder="Desc"></textarea>
-                <input name="image" type="text" placeholder="Name"/>
+                <textarea name="desc"ref={this.descRef} placeholder="Desc"></textarea>
+                <input name="image"ref={this.imageRef} type="text" placeholder="Image"/>
                 <button type="submit">+ Add Fish</button>
             </form>
         )
