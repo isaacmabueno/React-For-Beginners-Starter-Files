d382c1a3103e1b571f88f71032705fdd411488a1
isaacmabueno <isaacmabueno@gmail.com>
2018-10-24 11:58:38 -0500

    updating state and display stae with jsx

---
+++ catch-of-the-day/src/components/Fish.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+class Fish extends React.Component {
+    render() {
+        return <li className="menu-fish">
+        <img src={this.props.details.image} alt={this.props.details.name} />
+        <p>{this.props.details.name}</p>
+        </li>
+    }
+}
+
+export default Fish;
